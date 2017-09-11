47a60a68033c8c56f2e4d55e0c672a68d41b44f4
Max Kuhn <mxkuhn@gmail.com>
2017-09-10 21:10:14 -0400

    Added urls

--- R/ames_raw.R
+++ R/ames_raw.R
@@ -1,13 +1,17 @@
 #' Raw Ames Housing Data
 #'
-#' Summon the data described by De Cock (2011) where 82 fields were recored for 2,930 properties in Ames IA. 
+#' Summon the data described by De Cock (2011) where 82 fields were recored for 2,930 properties in Ames IA.
 #'
 #' @name ames_raw
 #' @aliases ames_raw
 #' @docType data
-#' @return \item{ames_raw}{a tibble} 
+#' @return \item{ames_raw}{a tibble}
 #'
 #' @source De Cock, D. (2011). "Ames, Iowa: Alternative to the Boston Housing Data as an End of Semester Regression Project," \emph{Journal of Statistics Education},  Volume 19, Number 3.
+#'
+#' \url{https://ww2.amstat.org/publications/jse/v19n3/decock/DataDocumentation.txt}
+#'
+#' \url{http://ww2.amstat.org/publications/jse/v19n3/decock.pdf}
 #'
 #' @keywords datasets
 NULL
